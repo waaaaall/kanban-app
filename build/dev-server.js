e4491510dcdc103591dc771fbcdeb57d89168b3f
waaaaall <nm7wall@gmail.com>
2019-07-18 16:42:07 +0900

    mock server setting

--- build/dev-server.js
+++ build/dev-server.js
@@ -5,4 +5,25 @@ const bodyParser = require('body-parser')
 module.exports = app => {
     app.use(bodyParser.json())
     //TODO APIの実装内容
+    const users = {
+        'foo@domain.com': {
+            password: '12345678',
+            userId: 1,
+            token:'1234567890abcdef'
+        }
+    }
+
+    app.post('/auth/login', (req, res) => {
+        const { email, password } = req.body
+        const user = users[email]
+        if (user) {
+            if (user.password !== password) {
+                res.status(401).json({ message: 'ログインに失敗しました。' })
+            } else {
+                res.json({userId:user.userId,token:user.token})
+            }
+        } else {
+            res.status(404).json({message:'ユーザーが登録されていません'})
+        }
+    })
 }
